d4244f5c2b4b184f089391683411cf1be17caa8a
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2020-10-08 10:00:19 +0300

    ...

--- ArrayExtensions.Tests/ArrayExtensionsTests.cs
+++ ArrayExtensions.Tests/ArrayExtensionsTests.cs
@@ -18,6 +18,7 @@ namespace Zaac.ArrayExtensions.Tests
 
             Assert.AreEqual(5, myArray.Last());
         }
+
         [TestMethod]
         public void ArrayFillExactDoubleTest()
         {
@@ -26,6 +27,20 @@ namespace Zaac.ArrayExtensions.Tests
             myArray.Fill(7);
 
             Assert.AreEqual(7, myArray.Last());
+        }
+
+        [TestMethod]
+        public void ArrayFillTestUnevens()
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                var myArray = new int[12001 + i];
+                var filler = new int[] { 0, 1, 2, 3, 4, 5 };
+
+                myArray.Fill(filler);
+
+                Assert.AreEqual(i, myArray.Last());
+            }
         }
 
     }
